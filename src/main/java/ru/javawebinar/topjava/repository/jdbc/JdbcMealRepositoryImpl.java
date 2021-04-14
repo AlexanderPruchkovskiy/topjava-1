deecda5ec5243a9ed9d7e22ac605c6bc506b5acd
AlexanderPruchkovskiy <Pruchkovskiy@gmail.com>
2021-04-14 12:38:42 +0300

    HW3_v2

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.CollectionUtils;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -72,7 +73,8 @@ else
     @Override
     public Meal get(int id, int userId) {
     String sqlStr="SELECT*FROM meals WHERE id=? AND user_id=?";
-        return DataAccessUtils.singleResult( jdbcTemplate.query(sqlStr,ROW_MAPPER,id,userId));
+    List<Meal> result=jdbcTemplate.query(sqlStr,ROW_MAPPER,id,userId);
+        return  DataAccessUtils.singleResult(result );
 
     }
 
